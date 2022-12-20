2a965ca07da8695850b70675e362ada73242ce83
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2022-12-20 14:44:42 +0530

    Added QR options and Css

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import qrcode
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.colormasks import ImageColorMask
 from PIL import Image
+import wifi_qrcode_generator as wifi
 
 app = Flask(__name__)
 
@@ -19,23 +20,135 @@ def generateQRImage(input, qrColor, qrBgColor):
     img = qr.make_image( fill_color=qrColor, back_color=qrBgColor)
     return img
 
+# Function to add Logo to QR Code
+def generateLogo(picture,input,qrColor,qrBgColor):
+    logo = Image.open(picture)
+    basewidth = 50
+    wpercent = (basewidth/float(logo.size[0]))
+    hsize = int((float(logo.size[1])*float(wpercent)))
+    logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+    img = generateQRImage(input,qrColor,qrBgColor)
+    pos = ( (img.size[0] - logo.size[0]) // 2, (img.size[1] - logo.size[1]) // 2)
+    img.paste(logo, pos)
+    return img
+
+# Function to save QR code
+def save(img,name):
+    img.save("./static/"+name)
+    return render_template('showQR.html', image = name)
 
 # Route for Index Page
 @app.route('/')
 def showQRForm():
     return render_template('index.html')
 
-
-# Function to generate Plain QR Code
-@app.route('/plainQRCode', methods = ['POST'])
-def generatePlainQRCode():
+# Function to generate QR Code for text
+@app.route('/text', methods = ['POST'])
+def generateTextQRCode():
     input = request.form['input']
     qrColor = request.form['qrColor']
     qrBgColor = request.form['qrBgColor']
+    picture  = request.files['picture']
+    img = ''
+    if(picture.filename != ''):
+        img = generateLogo(picture,input,qrColor,qrBgColor)
+    else:
+        img = generateQRImage(input,qrColor,qrBgColor)
+    return save(img,"text.png")
+
+
+# Function to generate QR Code for URL
+@app.route('/url', methods = ['POST'])
+def generateUrlQRCodeF():
+    input = request.form['input']
+    qrColor = request.form['qrColor']
+    qrBgColor = request.form['qrBgColor']
+    picture  = request.files['picture']
+    img = ''
+    if(picture.filename != ''):
+        img = generateLogo(picture,input,qrColor,qrBgColor)
+    else:
+        img = generateQRImage(input,qrColor,qrBgColor)
+    return save(img,"url.png")
+
+
+# Function to generate QR Code for Mail
+@app.route('/mail', methods = ['POST'])
+def generateMailQRCode():
+    mail = request.form['mail']
+    sub = request.form['sub']
+    body = request.form['body']
+    qrColor = request.form['qrColor']
+    qrBgColor = request.form['qrBgColor']
+    picture  = request.files['picture']
+
+    input = "mailto:"+mail+"?subject="+sub+"&body="+body
     
-    img = generateQRImage(input,qrColor,qrBgColor)
-    img.save("./static/plainQrCode.png")
-    return render_template('showQR.html', image = 'plainQrCode.png')
+    img = ''
+    if(picture.filename != ''):
+        img = generateLogo(picture,input,qrColor,qrBgColor)
+    else:
+        img = generateQRImage(input,qrColor,qrBgColor)
+    return save(img,"mail.png")
+
+
+# Function to generate QR Code for Whatsapp
+@app.route('/whatsapp', methods = ['POST'])
+def generateWhatsappQRCode():
+    whatsappNum = request.form['whatsappNum']
+    message = request.form['message']
+    qrColor = request.form['qrColor']
+    qrBgColor = request.form['qrBgColor']
+    picture  = request.files['picture']
+
+    input = "https://wa.me/+91"+whatsappNum+"?text="+message
+    img = ''
+    if(picture.filename != ''):
+        img = generateLogo(picture,input,qrColor,qrBgColor)
+    else:
+        img = generateQRImage(input,qrColor,qrBgColor)
+    return save(img,"whatsapp.png")
+
+# Function to generate QR Code for Telephone
+@app.route('/telephone', methods = ['POST'])
+def generateTelephoneQRCode():
+    telephoneNum = request.form['telephoneNum']
+    qrColor = request.form['qrColor']
+    qrBgColor = request.form['qrBgColor']
+    picture  = request.files['picture']
+
+    input = "tel:+91"+telephoneNum
+    img = ''
+    if(picture.filename != ''):
+        img = generateLogo(picture,input,qrColor,qrBgColor)
+    else:
+        img = generateQRImage(input,qrColor,qrBgColor)
+    return save(img,"telephone.png")
+
+# Function to generate QR Code for SMS
+@app.route('/sms', methods = ['POST'])
+def generateSmsQRCode():
+    mobileNum = request.form['mobileNum']
+    message = request.form['message']
+    qrColor = request.form['qrColor']
+    qrBgColor = request.form['qrBgColor']
+    picture  = request.files['picture']
+
+    input = "SMSTO:+91"+mobileNum+":"+message
+    img = ''
+    if(picture.filename != ''):
+        img = generateLogo(picture,input,qrColor,qrBgColor)
+    else:
+        img = generateQRImage(input,qrColor,qrBgColor)
+    return save(img,"sms.png")
+
+# Function to generate QR Code for Wifi
+@app.route('/wifi', methods = ['POST'])
+def generateWifiQRCode():
+    wifiId = request.form['wifi']
+    password = request.form['password']
+    img= wifi.wifi_qrcode(wifiId,False,"WPA",password)
+    return save(img,"wifi.png")
 
 
 # Function to generate QR Code from Picture/Logo
@@ -56,29 +169,6 @@ def generateQRCodeFromLogo():
     img = qr.make_image(image_factory=StyledPilImage, color_mask = ImageColorMask(back_color=(255,255,255), color_mask_image=logo))
     img.save('./static/QRCodeFromLogo.png')
     return render_template('showQR.html', image = 'QRCodeFromLogo.png')
-
-
-# Function to generate QR Code With Logo
-@app.route('/QRCodeWithLogo', methods = ['POST'])
-def generateQRCodeWithLogo():
-    input = request.form['input']
-    qrColor = request.form['qrColor']
-    qrBgColor = request.form['qrBgColor']
-    picture  = request.files['picture']
-
-    logo = Image.open(picture)
-   
-    basewidth = 50
-    wpercent = (basewidth/float(logo.size[0]))
-    hsize = int((float(logo.size[1])*float(wpercent)))
-    logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
-
-    img = generateQRImage(input,qrColor,qrBgColor)
-
-    pos = ( (img.size[0] - logo.size[0]) // 2, (img.size[1] - logo.size[1]) // 2)
-    img.paste(logo, pos)
-    img.save('./static/QRCodeWithLogo.png')
-    return render_template('showQR.html', image = 'QRCodeWithLogo.png')
 
 
 if __name__ == '__main__':
